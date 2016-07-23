39c2796bfd90fed71022917736de844d30f3aae7
David White <whited75@gmail.com>
2016-07-23 14:15:37 -0600

    Comment out code for committed files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react/addons';
+import React from 'react';
 import { render } from 'react-dom'; // I added this line
 import thunk from 'redux-thunk';
 import { combineReducers, createStore, applyMiddleware, bindActionCreators } from 'redux';
@@ -10,117 +10,126 @@ import App from './components/App'; // I added this line
 
 import jsonApiReducer from './json-api-reducer';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const reduxStore = createStoreWithMiddleware(combineReducers({
-  api: jsonApiReducer
-}));
+// The next two functions came from:
+//  https://github.com/reactjs/redux/blob/master/examples/todos/index.js
+let store = createStore(todoApp)
 
-reduxStore.dispatch({
-  type: 'DATA_LOAD',
-  data: [{
-    id: 'user-3903443433',
-    type: 'user',
-    attributes: {
-      email: 'carlosrr@gmail.com'
-    },
-    relationships: {
-      'favorite-movies': {
-        self: '/users/user-3903443433/movies/favorites',
-        related: '/users/user-3903443433/movies/favorites',
-      },
-      'people': {
-        self: '/users/user-3903443433/people',
-        related: '/users/user-3903443433/people',
-      }
-    }
-  }]
-});
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
-const App = React.createClass({
-  mixins: [React.addons.PureRenderMixin],
-  login(e) {
-    e.preventDefault();
-    this.props.login();
-  },
 
-  randomUser(e) {
-    e.preventDefault();
-    this.props.loadRandomUser();
-  },
+// Original code downloaded is below
 
-  renderTop(store) {
-    const currentUserId = store.getIn(['session', 'current-session', 'attributes', 'userId']);
+// reduxStore.dispatch({
+//   type: 'DATA_LOAD',
+//   data: [{
+//     id: 'user-3903443433',
+//     type: 'user',
+//     attributes: {
+//       email: 'carlosrr@gmail.com'
+//     },
+//     relationships: {
+//       'favorite-movies': {
+//         self: '/users/user-3903443433/movies/favorites',
+//         related: '/users/user-3903443433/movies/favorites',
+//       },
+//       'people': {
+//         self: '/users/user-3903443433/people',
+//         related: '/users/user-3903443433/people',
+//       }
+//     }
+//   }]
+// });
 
-    const user = selector(store, ['user', currentUserId]);
-    const currentUserEmail = user.attr('email');
-    return <div className="text-right">
-      Last updated {(new Date()).getTime()}
-      <span> | </span>
-      {currentUserEmail ? currentUserEmail : <em>No session</em>}
-      <span> | </span>
-      <a className="btn btn-info" onClick={this.randomUser}>Random user</a>
-      <span> | </span>
-      <a className="btn btn-info" onClick={this.login}>Log in</a>
-    </div>;
-  },
+// const App = React.createClass({
+//   mixins: [React.addons.PureRenderMixin],
+//   login(e) {
+//     e.preventDefault();
+//     this.props.login();
+//   },
 
-  renderRelationship(relationshipName) {
-    const {store} = this.props;
-    const currentUserId = store.getIn(['session', 'current-session', 'attributes', 'userId']);
-    const getProperties = (base, properties) => {
-      return properties.reduce((obj, propName) => {
-        obj[propName] = store.getIn(base.concat([propName]));
-        return obj;
-      }, {});
-    };
+//   randomUser(e) {
+//     e.preventDefault();
+//     this.props.loadRandomUser();
+//   },
 
-    const loadRelationship = (e) => {
-      e.preventDefault();
-      this.props.loadRelationship("user", currentUserId, relationshipName);
-    };
+//   renderTop(store) {
+//     const currentUserId = store.getIn(['session', 'current-session', 'attributes', 'userId']);
 
-    const r = ['relationships', 'user', currentUserId, relationshipName];
-    const {isLoadingPage, nextPageHref, data} = getProperties(r, ['data', 'nextPageHref', 'isLoadingPage']);
-    let loadMoreLink = 'Nothing to load';
-    if (isLoadingPage) {
-      loadMoreLink = 'loading...';
-    } else if (nextPageHref) {
-      loadMoreLink = <a href="#" onClick={loadRelationship}>load more...</a>;
-    }
+//     const user = selector(store, ['user', currentUserId]);
+//     const currentUserEmail = user.attr('email');
+//     return <div className="text-right">
+//       Last updated {(new Date()).getTime()}
+//       <span> | </span>
+//       {currentUserEmail ? currentUserEmail : <em>No session</em>}
+//       <span> | </span>
+//       <a className="btn btn-info" onClick={this.randomUser}>Random user</a>
+//       <span> | </span>
+//       <a className="btn btn-info" onClick={this.login}>Log in</a>
+//     </div>;
+//   },
 
-    return [
-      <h2>/user/{relationshipName} {loadMoreLink}</h2>,
-      <pre>{data &&  JSON.stringify(data.toJS(), null, '\t')}</pre>
-    ];
-  },
+//   renderRelationship(relationshipName) {
+//     const {store} = this.props;
+//     const currentUserId = store.getIn(['session', 'current-session', 'attributes', 'userId']);
+//     const getProperties = (base, properties) => {
+//       return properties.reduce((obj, propName) => {
+//         obj[propName] = store.getIn(base.concat([propName]));
+//         return obj;
+//       }, {});
+//     };
 
-  render() {
-    const {store} = this.props;
-    return <div>
-      {this.renderTop(store)}
-      <div className="row">
-        <div className="col-sm-5 col-sm-offset-1">
-          <h2>Store</h2>
-          <pre>{JSON.stringify(store.toJS(), null, ' ')}</pre>
-        </div>
-        <div className="col-sm-3">
-          {this.renderRelationship("favorite-movies", "movie")}
-        </div>
-        <div className="col-sm-3">
-          {this.renderRelationship("people", "artist")}
-        </div>
-      </div>
-    </div>;
-  }
-});
+//     const loadRelationship = (e) => {
+//       e.preventDefault();
+//       this.props.loadRelationship("user", currentUserId, relationshipName);
+//     };
 
-const SmartApp = connect((state) => {
-  return { store: state.api };
-}, (dispatch) => bindActionCreators(actionCreators, dispatch)
-)(App, { store: reduxStore });
+//     const r = ['relationships', 'user', currentUserId, relationshipName];
+//     const {isLoadingPage, nextPageHref, data} = getProperties(r, ['data', 'nextPageHref', 'isLoadingPage']);
+//     let loadMoreLink = 'Nothing to load';
+//     if (isLoadingPage) {
+//       loadMoreLink = 'loading...';
+//     } else if (nextPageHref) {
+//       loadMoreLink = <a href="#" onClick={loadRelationship}>load more...</a>;
+//     }
 
-window.onload = () => {
-  React.render(<Provider store={reduxStore}>
-    {() => <SmartApp />}
-  </Provider>, document.querySelector('#container'));
-};
+//     return [
+//       <h2>/user/{relationshipName} {loadMoreLink}</h2>,
+//       <pre>{data &&  JSON.stringify(data.toJS(), null, '\t')}</pre>
+//     ];
+//   },
+
+//   render() {
+//     const {store} = this.props;
+//     return <div>
+//       {this.renderTop(store)}
+//       <div className="row">
+//         <div className="col-sm-5 col-sm-offset-1">
+//           <h2>Store</h2>
+//           <pre>{JSON.stringify(store.toJS(), null, ' ')}</pre>
+//         </div>
+//         <div className="col-sm-3">
+//           {this.renderRelationship("favorite-movies", "movie")}
+//         </div>
+//         <div className="col-sm-3">
+//           {this.renderRelationship("people", "artist")}
+//         </div>
+//       </div>
+//     </div>;
+//   }
+// });
+
+// const SmartApp = connect((state) => {
+//   return { store: state.api };
+// }, (dispatch) => bindActionCreators(actionCreators, dispatch)
+// )(App, { store: reduxStore });
+
+// window.onload = () => {
+//   React.render(<Provider store={reduxStore}>
+//     {() => <SmartApp />}
+//   </Provider>, document.querySelector('#container'));
+// };
--- src/selector.js
+++ src/selector.js
@@ -1,14 +1,14 @@
-class Selector {
-  constructor(state, path) {
-    this.state = state;
-    this.path = path;
-  }
+// class Selector {
+//   constructor(state, path) {
+//     this.state = state;
+//     this.path = path;
+//   }
 
-  attr(attributeName) {
-    return this.state.getIn(this.path.concat(['attributes', attributeName]));
-  }
-}
+//   attr(attributeName) {
+//     return this.state.getIn(this.path.concat(['attributes', attributeName]));
+//   }
+// }
 
-export default function(state, path) {
-  return new Selector(state, path);
-}
+// export default function(state, path) {
+//   return new Selector(state, path);
+// }
